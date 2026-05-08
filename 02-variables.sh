b15c81402b027a0d54f24b5046315220c93ddeb1
umamahesh <umaheshe25@gmail.com>
2026-05-09 03:57:52 +0530

    Added a conversation

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Uma:: Hi Mahesh"
+echo "Uma:: I am learning bash scripting"
+echo "Mahesh:: That's great Uma, keep it up!"
+echo "Uma:: Thank you Mahesh, I will!"
+echo "Mahesh:: Do you have any questions about bash scripting?"
