f1af24dd81dff83f018b65b33a542aa574739df9
umamahesh <umaheshe25@gmail.com>
2026-05-09 04:51:00 +0530

    Now we can change names

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hi ${PERSON2}"
+echo "${PERSON1}:: I am learning bash scripting"
+echo "${PERSON2}:: That's great ${PERSON1}, keep it up!"
+echo "${PERSON1}:: Thank you ${PERSON2}, I will!"
+echo "${PERSON2}:: Do you have any questions about bash scripting?"
