f2ebda4a8951e8a266a3644dd2662b91fadd7604
umamahesh <umaheshe25@gmail.com>
2026-05-10 15:42:24 +0530

    Added Array example

---
+++ 07-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Movies=("Inception" "The Matrix" "Interstellar")
+echo "First movie: ${Movies[0]}"
+echo "Second movie: ${Movies[1]}"
+echo "Third movie: ${Movies[2]}"
+echo "All movies: ${Movies[@]}"
